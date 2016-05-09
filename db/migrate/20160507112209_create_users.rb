ec8fb240267574c847d4ae0c33a2014c63625090
nbkhope <ryumemaru@yahoo.com>
2016-05-08 21:31:11 -0700

    Add validations to User model to require (unique) username and password; also add index to migration so that the same username cannot be used twice to create a record

--- db/migrate/20160507112209_create_users.rb
+++ db/migrate/20160507112209_create_users.rb
@@ -1,10 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string  :username
-      t.string  :password_hash
+      t.string  :username, null: false
+      t.string  :password_hash, null: false
 
       t.timestamps  null: false
     end
+
+    # Make the username unique 
+    # (so you cannot create multiple accounts with the same username)
+    add_index :users, :username, unique: true
   end
 end
